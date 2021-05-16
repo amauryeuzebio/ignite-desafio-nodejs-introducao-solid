a0e253546dabccaed2d320daf7ff5a5f48a94b8e
Amaury Euzebio <amaury@a1tecnologia.info>
2021-05-15 23:16:26 -0300

    Resolução desafio Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import Swagger from "swagger-ui-express";
 
+import SwaggerFile from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", Swagger.serve, Swagger.setup(SwaggerFile));
 
 app.use("/users", usersRoutes);
 
